29a2a9589df3d466a17413d0209a3c3005cbcfc0
Muhammad Bin Riaz <muhammadbinriaz675@gmail.com>
2026-04-05 17:58:28 +0500

    order thing and status code stuff solved

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
@@ -32,16 +32,28 @@ def get_posts():
 	return {"data": my_posts}
 
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_post(post: Post):
 	post_dict = post.model_dump()
 	post_dict['id'] = randrange(0, 1000000)
 	my_posts.append(post_dict)
 	return {"data": post_dict}
 
+# there is inter collision between /posts/latest_post and /posts/{id}, so order matters and we put latest_post route above because of that
+
+@app.get("/posts/latest_post")
+def get_latest_post():
+  post = my_posts[int(len(my_posts)) - 1]
+  print(type(post))
+  return {"details": post}
 
 @app.get("/posts/{id}")
-def get_post(id: int):
+def get_post(id: int, response: Response):
 
   post = find_post(id)
+  if not post:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"page with id {id} was not found bruh!")
+    #response.status_code = status.HTTP_404_NOT_FOUND
+    #return {"message": f"page with id {id} was not found bruh!"}
   return {"message": post}
+
