8a5009f9e8483689f28d88a73ab841a8a3f4edaa
Muhammad Bin Riaz <muhammadbinriaz675@gmail.com>
2026-04-07 19:58:06 +0500

    making separate file of schemas, in main we addup what we wanna show back as response by adding response_model in app decorator

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
 from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
 from sqlalchemy.orm import Session
-from  . import models 
+from  . import models, schemas
 from .database import engine, get_db
 
 models.Base.metadata.create_all(bind=engine)
@@ -15,11 +15,6 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-
-class Post(BaseModel):
-	title: str
-	content: str
-	published: bool = True
 
 while True:	
 
@@ -53,38 +48,32 @@ def root():
   return {"message": "welcome to my api"}
 
 
-@app.get("/sqlalchemy")
-def test_posts(db: Session = Depends(get_db)):
-  posts = db.query(models.Post).all()
-  print(posts)
-  return {"data": "succhess"}
 
-
-@app.get("/posts")
+@app.get("/posts", response_model=list[schemas.Post])
 def get_posts(db: Session = Depends(get_db)):
 
   posts = db.query(models.Post).all()
-  return {"data": posts}
+  return posts
 
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_post(post: Post, db: Session = Depends(get_db)):
+@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
 
   new_post = models.Post(**post.model_dump())
   db.add(new_post)
   db.commit()
   db.refresh(new_post)
-  return {"data": new_post}
+  return new_post
 
 
-@app.get("/posts/{id}")
+@app.get("/posts/{id}", response_model=schemas.Post)
 def get_post(id: int, response: Response,  db: Session = Depends(get_db)):
 
   post = db.query(models.Post).filter(models.Post.id == id).first()
 
   if not post:
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"page with id {id} was not found!")
-  return {"message": post}
+  return post
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)	
@@ -100,8 +89,8 @@ def delete_post(id: int, db: Session = Depends(get_db)):
   return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
-@app.put("/posts/{id}")
-def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
+@app.put("/posts/{id}", response_model=schemas.Post)
+def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
 
   post_query = db.query(models.Post).filter(models.Post.id == id)
   post = post_query.first()
@@ -112,4 +101,4 @@ def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
   post_query.update(dict(updated_post), synchronize_session=False)
   db.commit()
 
-  return {"data": post_query.first()}
+  return post_query.first()
