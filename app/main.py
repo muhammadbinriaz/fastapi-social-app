3d3de9ccff8f3b5b512824800993edc4c7f07eff
Muhammad Bin Riaz <muhammadbinriaz675@gmail.com>
2026-06-11 14:01:20 +0500

    made some tweaks for live reload if we do any changes in app

--- app/main.py
+++ app/main.py
@@ -12,3 +12,8 @@ app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
+
+
+@app.get("/")
+async def root():
+  return {"message": "Hello World 1"}
